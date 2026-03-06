5d56bbce08ac59420c2aedc10f8dec5d8652757e
Vitaliy-Berezhnoy <vitaliyvitberezhnoy@gmail.com>
2026-03-06 19:48:56 +0300

    Add function to select a handler for a POST request

---
+++ includes/route-post-actions.php
@@ -0,0 +1,206 @@
+<?php
+
+//  Функция определяет обработчик для POST-запроса и, если нужно, выполняет редирект после обработки.
+function route_post_actions() {
+
+    //  Проверяем наличие POST запроса.
+    if (!isset($_SERVER['REQUEST_METHOD']) || $_SERVER['REQUEST_METHOD'] !== 'POST') {
+        return false;
+    }
+
+    //  Выбираем функцию обработчик POST запроса.
+    $result = match (true) {
+        isset($_POST['save_db_choice']) => handle_db_switch_request(),  //  запрос от формы переключения БД
+        isset($_POST['submit_comment']) => process_and_save_comment(),  //  запрос от формы ввода комментария
+        isset($_POST['delete_selected_comments']) => process_comment_deletion_request(), // от формы удаления комментариев
+        isset($_POST['confirm_deletion_submit']) => handle_comment_deletion_confirmation(),  // от формы подтверждения удаления
+        default => false,        
+    };
+    
+    if ($result) {
+        // Редирект ДО начала вывода контента
+        wp_safe_redirect($_SERVER['REQUEST_URI']);
+        exit;
+    }
+    //  Редирект не нужен
+    return false;
+}
+
+
+// Функция для обработки POST запроса от формы переключения БД
+function handle_db_switch_request() {
+
+    // Проверяем nonce
+    if (!isset($_POST['save_db_choice_nonce']) || !wp_verify_nonce($_POST['save_db_choice_nonce'], 'save_db_choice_action')) {
+        return false;
+    }
+
+    $name_selected_db = sanitize_text_field($_POST['db_choice']);
+
+    // Сохраняем в cookie (на 3 дня)
+    setcookie(
+        'current_db',
+        $name_selected_db,
+        time() + (86400 * 3),
+        '/',
+        '',
+        false,
+        true
+    );
+
+    return true;  // true - разрешит Редирект
+}
+
+
+// Функция для обработки POST запроса от формы ввода комментария
+function process_and_save_comment() {
+
+    // Проверяем nonce
+    if (!isset($_POST['add_comment_nonce']) || !wp_verify_nonce($_POST['add_comment_nonce'], 'add_comment_action')) {
+        return false;
+    }
+
+    // Санизируем данные
+    $name = sanitize_text_field($_POST['comment_name']);
+    $comment = sanitize_textarea_field($_POST['comment_text']);
+
+    //  Проверяем обязательные поля
+    if (empty($name) || empty($comment)) {
+        //  Сохраняем сообщение в transient
+        set_transient(
+            'comment_status',
+            [
+                'type' => 'warning',
+                'message' => 'Заполните все поля!'
+            ],
+            180  // время жизни в секундах
+        );
+        return false;  // false - запретит Редирект
+    }
+
+    // Сохраняем комментарий в одну из БД PostgreSQL или MySQL
+    $table_name = get_table_name();
+    $pdo = get_pdo_active_db();
+
+    $sql = "INSERT INTO $table_name (name, comment) VALUES (:name, :comment);";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':name', $name, PDO::PARAM_STR);
+    $stmt->bindParam(':comment', $comment, PDO::PARAM_STR);
+
+    try {
+        $stmt->execute();
+        $comment_status = [
+            'type' => 'success',
+            'message' => 'Комментарий добавлен!'
+        ];
+    } catch(PDOException $e) {
+        error_log(message: "Error writing a comment to the database: " . $e);
+        $comment_status = [
+            'type' => 'error',
+            'message' => 'Ошибка при записи комментария в БД!'
+        ];
+    }
+    // Закрываем соеденение
+    $pdo = null;
+    $stmt = null;
+
+    // Сохраняем сообщение об успехе/ошибке в transient
+    set_transient('comment_status', $comment_status, 180);  // время жизни 180 секунд
+
+    return true;    // true - разрешит Редирект
+}
+
+
+//  Функция для обработки POST‑запроса от формы удаления комментариев
+function process_comment_deletion_request() {
+
+    //  Проверяем nonce
+    if (!isset($_POST['delete_comments_nonce']) || !wp_verify_nonce($_POST['delete_comments_nonce'], 'delete_comments_action')) {
+        return false;
+    }
+
+    //  Получаем список комментариев помеченных на удаление
+    $comment_ids = $_POST['comment_ids'] ?? [];
+    $comment_ids = array_map('intval', $comment_ids);
+
+    //  Сохраняем в transient на три минуты
+    set_transient('comment_ids', $comment_ids, 180);
+
+    return true;    // true - разрешит Редирект
+}
+
+
+//  Функция для обработки POST‑запроса от формы подтверждения удаления
+function handle_comment_deletion_confirmation() {
+
+    //  Проверяем что это nonce от формы подтверждения удаления
+    if (!isset($_POST['confirmation_of_deletion_nonce']) ||
+        !wp_verify_nonce($_POST['confirmation_of_deletion_nonce'], 'confirmation_of_deletion_action')) {
+
+        return false;
+    }
+
+    // Извлекаем ID
+    $selected_ids = $_POST['selected_ids'] ?? [];
+
+    if (empty($selected_ids)) {
+        set_transient(
+            'comment_status',
+            [
+                'type' => 'warning',
+                'message' => 'Не выбраны комментарии для удаления'
+            ],
+            180  // время жизни в секундах
+        );
+
+        return true;    // true - разрешит Редирект
+    }
+
+    //  Санизируем ID
+    $selected_ids = array_map('intval', $selected_ids);    // преабразует в числа
+    $selected_ids = array_filter($selected_ids);    // удалит пустые элементы
+
+    if (empty($selected_ids)) {
+        set_transient(
+            'comment_status',
+            [
+                'type' => 'error',
+                'message' => 'Ошибка при удалении - Некорректные ID комментариев'
+            ],
+            180
+        );
+
+        return true;   // true - разрешит Редирект
+    }
+
+    // Удаляем комментарии
+
+    $pdo = get_pdo_active_db();
+
+    $table_name = get_table_name();
+    $placeholders = str_repeat('?, ', count($selected_ids) - 1) . '?';
+    $sql = "DELETE FROM $table_name WHERE id IN ($placeholders)";
+    $stmt = $pdo->prepare($sql);
+    try {
+        $stmt->execute($selected_ids);
+        $deleted_count = $stmt->rowCount();
+        $comment_status = [
+            'type' => 'success',
+            'message' => "Удалено комментариев: {$deleted_count}"
+        ];
+    } catch(PDOException $e) {
+        error_log("Error when deleting a comment from the database: {$e}");
+        $comment_status = [
+            'type' => 'error',
+            'message' => "Ошибка при удалении комментариев"
+        ];
+    }
+    // Закрываем соеденение с БД
+    $stmt = null;
+    $pdo = null;
+
+    // Сохраняем сообщение об успехе/ошибке в transient
+    set_transient('comment_status', $comment_status, 180);  // время жизни 180 секунд
+
+    return true;   // true - разрешит Редирект
+}
--- my-comments-plugin.php
+++ my-comments-plugin.php
@@ -10,10 +10,10 @@
 const TABLE_NAME = 'proposals_and_comments';
 
 require_once plugin_dir_path(__FILE__) . 'includes/table_name.php';
-//require_once plugin_dir_path(__FILE__) . 'Database/PG_DB_Handler.php';
 require_once plugin_dir_path(__FILE__) . 'includes/pdo_connect.php';
 require_once plugin_dir_path(__FILE__) . 'includes/selected_db.php';
-require_once plugin_dir_path(__FILE__) . 'includes/comment_deletion.php';
+require_once plugin_dir_path(__FILE__) . 'includes/route-post-actions.php';
+require_once plugin_dir_path(__FILE__) . 'includes/forms-and-tables.php';
 
 // Создаём таблицу в БД Mysql при активации плагина
 if ( !function_exists('create_table_in_mysql')) {
@@ -27,14 +27,9 @@ if (!function_exists('create_table_in_postgresql')) {
 }
 register_activation_hook( __FILE__, 'create_table_in_postgresql' );
 
-// Обработка комментария
-add_action('init', 'process_and_save_comment');
+//  Обрабатываем POST запросы
+add_action('init', 'route_post_actions');
 
-// Обработка переключателя БД
-add_action('init', 'handle_db_switch_request');
-
-add_action('init', 'process_comment_deletion_request');
-add_action('init', 'handle_comment_deletion_confirmation');
 
 // Подключаем bootstrap-local
 add_action('wp_enqueue_scripts', function() {
@@ -49,139 +44,6 @@ add_action('wp_enqueue_scripts', function() {
 
 add_shortcode('show_comments', 'comments_shortcode');
 
-// Функция для обработки POST запроса от формы ввода комментария
-function process_and_save_comment() {
-    // Проверяем, что это POST запрос с нашей формы
-    if ($_SERVER['REQUEST_METHOD'] !== 'POST' || !isset($_POST['submit_comment'])) {
-        return false;
-    }
-
-    // Проверяем nonce
-    if (!isset($_POST['add_comment_nonce']) || !wp_verify_nonce($_POST['add_comment_nonce'], 'add_comment_action')) {
-        return false;
-    }
-
-    // Санизируем данные
-    $name = sanitize_text_field($_POST['comment_name']);
-    $comment = sanitize_textarea_field($_POST['comment_text']);
-
-    //  Проверяем обязательные поля
-    if (empty($name) || empty($comment)) {
-        set_transient(
-            'comment_status',
-            [
-                'type' => 'warning',
-                'message' => 'Заполните все поля!'
-            ],
-            180  // время жизни в секундах
-        );
-        return false;
-    }
-
-    // Сохраняем комментарий в одну из БД PostgreSQL или MySQL
-    $table_name = get_table_name();
-    $pdo = get_pdo_active_db();
-
-    $sql = "INSERT INTO $table_name (name, comment) VALUES (:name, :comment);";
-    $stmt = $pdo->prepare($sql);
-    $stmt->bindParam(':name', $name, PDO::PARAM_STR);
-    $stmt->bindParam(':comment', $comment, PDO::PARAM_STR);
-
-    try {
-        $stmt->execute();
-        $comment_status = [
-            'type' => 'success',
-            'message' => 'Комментарий добавлен!'
-        ];
-        $pdo = null;
-        $stmt = null;
-    } catch(PDOException $e) {
-        error_log(message: "Error writing a comment to the database: " . $e);
-        $comment_status = [
-            'type' => 'error',
-            'message' => 'Ошибка при записи комментария в БД!'
-        ];
-        $pdo = null;
-        $stmt = null;
-    }
-
-    // Временно сохраняем сообщение об успехе/ошибке
-    set_transient('comment_status', $comment_status, 180);  // время жизни 180 секунд
-
-    // Редирект ДО начала вывода контента
-    wp_safe_redirect($_SERVER['REQUEST_URI']);
-    exit;
-}
-
-// Функция для обработки POST запроса от формы переключения БД
-function handle_db_switch_request() {
-    if (isset($_POST['save_db_choice']) && wp_verify_nonce($_POST['save_db_choice_nonce'], 'save_db_choice_action')) {
-        $selected_db = sanitize_text_field($_POST['db_choice']);
-
-        // Сохраняем в cookie (на 3 дня)
-        setcookie(
-            'current_db',
-            $selected_db,
-            time() + (86400 * 3),
-            '/',
-            '',
-            false,
-            true
-        );
-
-        // Редирект ДО начала вывода контента
-        wp_safe_redirect($_SERVER['REQUEST_URI']);
-        exit;
-    }
-    return false;
-}
-
-// Функция для отображения таблицы
-function display_comments_table($table_id = 'comments-table', $per_page = 5) {
-    $table_name = get_table_name();
-
-    // Определяем текущую страницу, учитывая оба формата URL
-    if (get_query_var('paged')) {
-        $current_page = get_query_var('paged');
-    } elseif (get_query_var('page')) {
-        $current_page = get_query_var('page');
-    } else {
-        $current_page = 1;
-    }
-    $current_page = max(1, intval($current_page));
-
-    // Определяем смещение для SQL-запроса
-    $offset = ($current_page - 1) * $per_page;
-
-    // Определяем общее количество строк в таблице
-    $pdo = get_pdo_active_db();
-    $sql1 = "SELECT COUNT(*) AS total FROM $table_name";
-    $stmt = $pdo->query($sql1);
-    $row = $stmt->fetch(PDO::FETCH_ASSOC);
-    $total = $row ? (int)$row['total'] : 0;
-
-    // Получаем комментарии с лимитом
-    $sql2 = "SELECT * FROM $table_name
-             ORDER BY created_at DESC
-             LIMIT :limit OFFSET :offset";
-    $stmy2 = $pdo->prepare($sql2);
-    $stmy2->bindParam(':limit', $per_page, PDO::PARAM_INT);
-    $stmy2->bindParam(':offset', $offset, PDO::PARAM_INT);
-    $stmy2->execute();
-    $comments = $stmy2->fetchAll(PDO::FETCH_OBJ);
-
-
-    // Передаем данные для пагинации в шаблон
-    $pagination_data = [
-        'total' => $total,
-        'per_page' => $per_page,
-        'current_page' => $current_page
-    ];
-
-    include plugin_dir_path(__FILE__) . 'templates/comments-table.php';
-
-    return true;
-}
 
 function comments_shortcode() {
     ob_start();
